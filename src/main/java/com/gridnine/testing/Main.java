f7b3534b4958ee6109304490f2e2180f960cfefd
Evgeny Bayandin <5087081@gmail.com>
2024-06-14 19:07:20 +0300

    Update Main.java

--- src/main/java/com/gridnine/testing/Main.java
+++ src/main/java/com/gridnine/testing/Main.java
@@ -9,6 +9,12 @@ public class Main {
         List<Flight> flights = FlightBuilder.createFlights();
         System.out.println("All flights: ");
         flights.forEach(System.out::println);
-        
+
+        // Flights after filtering out flights departing before current time:
+        FlightFilter departureBeforeCurrentTimeFilter = new DepartureBeforeCurrentTimeFilter();
+        System.out.println("\nFlights after filtering out flights departing before current time:");
+        List<Flight> filteredFlights= departureBeforeCurrentTimeFilter.filterFlights(flights);
+        filteredFlights.forEach(System.out::println);
+
     }
 }
